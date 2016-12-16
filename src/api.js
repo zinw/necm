f81183db78f1e1c3e004dc0d68b942784f096e7c
Zinway Liu <zinway.liu@ucloud.cn>
2016-12-16 18:37:37 +0800

    necm.api <5/N>

--- src/api.js
+++ src/api.js
@@ -1,110 +1,124 @@
 /**
  * Created by Zinway on 2016/12/9.
  */
-import request from 'request'
+import request from 'request-promise-native'
 import {origin, globalOption} from './config'
 import {deepCopy} from './util'
 
-const song = (id, callback = null) => {
-    const option = deepCopy(globalOption)
-    const url = `${origin}/api/song/detail?ids=%5B${id}%5d`
-    const method = 'GET'
-    Object.assign(option, {url, method})
-    request(option, (err, res, body) => {
-        if (!err && res.statusCode == 200) {
-            let info = JSON.parse(body);
-            callback && callback(JSON.stringify(info, '', 2))
-        } else {
-            console.error(err)
-        }
-    })
+/**
+ * 根据id获取歌曲
+ * @param {number} id 歌曲id
+ */
+const song = (id) => {
+    const option = deepCopy(globalOption);
+    const url = `${origin}/api/song/detail?ids=%5B${id}%5d`;
+    const method = 'GET';
+    Object.assign(option, {url, method});
+    return request(option)
+        .then(body => {
+            return JSON.parse(body).songs
+        })
+        .catch(e => e);
 };
 
-const search = (name = null, callback = null, onlySong = true, limit = 3, offset = 0) => {
-    const option = deepCopy(globalOption)
-    const url = `${origin}/api/search/suggest/web`
+/**
+ * 搜索
+ * @param {string} name 搜索内容
+ * @param {number} type 搜索类型：单曲(1)，歌手(100)，专辑(10)，歌单(1000)，用户(1002)
+ * @param {boolean} onlySong 仅显示歌曲，不要MV
+ * @param {number} limit 返回条数
+ * @param {number} offset 分页偏移量
+ */
+const search = (name = null, type = 1, onlySong = true, limit = 3, offset = 0) => {
+    const option = deepCopy(globalOption);
+    const url = `${origin}/api/search/suggest/web`;
     const form = {
         s: name,
+        type,
         limit,
-        type: 1,
         offset
-    }
-    const method = 'POST'
-    Object.assign(option, {url, form, method})
-    request(option, (err, res, body) => {
-        if (!err && res.statusCode == 200) {
-            let info = JSON.parse(body)
-            let data
+    };
+    const method = 'POST';
+    Object.assign(option, {url, form, method});
+    return request(option)
+        .then(body => {
+            let info = JSON.parse(body);
+            let data;
             if (onlySong) {
                 data = info.result.songs
             } else {
                 data = {songs: info.result.songs, mvs: info.result.mvs}
             }
-            callback && callback(JSON.stringify(data, '', 2))
-        } else {
-            console.error(err)
-        }
-    })
+            return data
+        })
+        .catch(e => e);
 };
 
-const lrc = (id, callback = null, lv = -1) => {
-    const option = deepCopy(globalOption)
-    const url = `${origin}/api/song/lyric?lv=${lv}&id=${id}`
-    const method = 'GET'
-    Object.assign(option, {url, method})
-    request(option, (err, res, body) => {
-        if (!err && res.statusCode == 200) {
-            let info = JSON.parse(body)
-            callback && callback(JSON.stringify(info, '', 2))
-        } else {
-            console.error(err)
-        }
-    })
+/**
+ * 根据id获取歌单
+ * @param {number} id 歌单id
+ * @param {number} lv
+ */
+const lrc = (id, lv = -1) => {
+    const option = deepCopy(globalOption);
+    const url = `${origin}/api/song/lyric?lv=${lv}&id=${id}`;
+    const method = 'GET';
+    Object.assign(option, {url, method});
+    return request(option)
+        .then(body => {
+            return JSON.parse(body).lrc.lyric
+        })
+        .catch(e => e);
 };
 
-const playLists = (id, callback) => {
-    const option = deepCopy(globalOption)
-    const url = `${origin}/api/playlist/detail?id=${id}`
-    const method = 'get'
-    Object.assign(option, {url, method})
-    request(option, (err, res, body) => {
-        if (!err && res.statusCode == 200) {
-            let info = JSON.parse(body)
-            callback && callback(JSON.stringify(info, '', 2))
-        } else {
-            console.error(err)
-        }
-    })
+/**
+ * 根据id获取歌单
+ * @param {number} id 歌单id
+ */
+const playLists = (id) => {
+    const option = deepCopy(globalOption);
+    const url = `${origin}/api/playlist/detail?id=${id}`;
+    const method = 'GET';
+    Object.assign(option, {url, method});
+    return request(option)
+        .then(body => {
+            return JSON.parse(body).result
+        })
+        .catch(e => e);
 };
 
-const artistAlbums = (id, callback, limit = 3, offset = 0) => {
-    const option = deepCopy(globalOption)
-    const url = `${origin}/api/artist/albums/${id}?offset=${offset}&limit=${limit}`
-    const method = 'GET'
-    Object.assign(option, {url, method})
-    request(option, (err, res, body) => {
-        if (!err && res.statusCode == 200) {
-            let info = JSON.parse(body)
-            callback && callback(JSON.stringify(info, '', 2))
-        } else {
-            console.error(err)
-        }
-    })
+/**
+ * 根据歌手id获取专辑
+ * @param {number} id 歌手id
+ * @param {number} limit 返回条数
+ * @param {number} offset 分页偏移量
+ */
+const artistAlbums = (id, limit = 3, offset = 0) => {
+    const option = deepCopy(globalOption);
+    const url = `${origin}/api/artist/albums/${id}?offset=${offset}&limit=${limit}`;
+    const method = 'GET';
+    Object.assign(option, {url, method});
+    return request(option)
+        .then(body => {
+            return JSON.parse(body)
+        })
+        .catch(e => e);
 };
 
-const albums = (id, callback) => {
-    const option = deepCopy(globalOption)
-    const url = `${origin}/api/album/${id}`
-    const method = 'get'
-    Object.assign(option, {url, method})
-    request(option, (err, res, body) => {
-        if (!err && res.statusCode == 200) {
-            let info = JSON.parse(body)
-            callback && callback(JSON.stringify(info, '', 2))
-        } else {
-            console.error(err)
-        }
-    })
+/**
+ * 根据id获取专辑
+ * @param {number} id 专辑id
+ */
+const albums = (id) => {
+    const option = deepCopy(globalOption);
+    const url = `${origin}/api/album/${id}`;
+    const method = 'GET';
+    Object.assign(option, {url, method});
+    return request(option)
+        .then(body => {
+            return JSON.parse(body).album
+        })
+        .catch(e => e);
 };
 
 
--- test/api.test.js
+++ test/api.test.js
@@ -4,39 +4,33 @@
 import api from '../src/api'
 
 describe('API测试', () => {
-    it('api.search测试', () => {
-        api.search("演员", data => {
-            expect(data[0].name).toBe("演员")
-        })
+    it('api.search测试', async () => {
+        let d = await api.search("演员")
+        expect(d[0].name).toBe('演员')
     })
 
-    it('api.song测试', () => {
-        api.song('32507038', data => {
-            expect(data.songs[0].name).toBe('演员')
-        })
+    it('api.song测试', async () => {
+        let d = await api.song('32507038')
+        expect(d[0].name).toBe('演员')
     })
 
-    it('api.lrc测试', () => {
-        api.lrc('32507038', data => {
-            expect(data.code).toBe(200)
-        })
+    it('api.lrc测试', async () => {
+        let d = await api.lrc('32507038')
+        expect(d).not.toBeNull()
     })
 
-    it('api.playLists测试', () => {
-        api.playLists('1', data => {
-            expect(data.code).toBe(200)
-        })
+    it('api.playLists测试',async () => {
+        let d = await api.playLists('1')
+        expect(d.id).toBe(1)
     })
 
-    it('api.artistAlbums测试', () => {
-        api.artistAlbums('5781', data => {
-            expect(data.code).toBe(200)
-        })
+    it('api.artistAlbums测试', async () => {
+        let d = await api.artistAlbums('5781')
+        expect(d.artist.name).toBe('薛之谦')
     })
 
-    it('api.albums测试', () => {
-        api.albums('3154175', data => {
-            expect(data.code).toBe(200)
-        })
+    it('api.albums测试', async () => {
+        let d = await api.albums('3154175')
+        expect(d.name).toBe('绅士')
     })
 });
