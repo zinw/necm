77567d6e82693ad16bd67f8c1c6f69aa87116641
Zinway Liu <zinway.liu@ucloud.cn>
2016-12-23 17:20:56 +0800

    拉取具体排行榜歌曲内容

--- src/menu.js
+++ src/menu.js
@@ -3,11 +3,12 @@
  */
 import Ui from './ui'
 import api from './api'
+import Player from './player'
 
 class Menu extends Ui {
     constructor() {
         super();
-        this._bind('initTopList');
+        this._bind('initTopList', 'topListDo');
         this.mainMenuList = [
             ['排行榜', this.initTopList]
         ];
@@ -22,7 +23,6 @@ class Menu extends Ui {
         this.mainMenuList[index][1]();
     }
 
-    // 主菜单
     initMainMenu() {
         this.list.setItems(this.getMainMenuListNames());
         this.screen.render();
@@ -33,12 +33,33 @@ class Menu extends Ui {
         });
     }
 
-    // 排行榜
+    async topListDo(index = 0) {
+        this.playList = await api.getTopSongList(index);
+        this.songNames = this.playList.map((s, i) => {
+            const {name, artists, album} = s;
+            let _artists = artists.map(a => a.name).join(' & '),
+                _album = album.name;
+            return ` ${i}. ${name} - ${_artists} <${_album}>`;
+        });
+        this.list.setItems(this.songNames);
+        this.screen.render();
+        this.songsList = this.playList.map(async(s) => {
+            let {id} = s;
+            let song = await api.song(id);
+            return song
+        });
+        let player = new Player(this.songsList);
+        this.list.on('select', (item, index) => {
+            player.play(index)
+        });
+    }
+
     initTopList() {
         this.list.setItems(api.getTopListNames());
         this.screen.render();
         this.list.once('select', (item, index) => {
             this.list.setLabel(` ${item.getText().split('.').slice(-1)[0].trim()} `);
+            this.topListDo(index);
             this.screen.render();
         });
     }
