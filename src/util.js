1778ea97625804a38e587585adf331d2b154d0c4
Zinway Liu <zinway.liu@ucloud.cn>
2016-12-24 00:58:32 +0800

    add sync-request

--- src/util.js
+++ src/util.js
@@ -1,6 +1,37 @@
 /**
  * Created by Zinway on 2016/12/7.
  */
+import request from 'sync-request'
+
 const deepCopy = obj => JSON.parse(JSON.stringify(obj));
 
-export {deepCopy}
+/**
+ * [function] 同步执行request get
+ * @param {string} url
+ * @param {object} options
+ * @return {object} body
+ */
+const requestGet = (url, options = {}) => {
+    return JSON.parse(request('GET', url, options).getBody('utf8'))
+};
+
+const requestGetRaw = (url, options = {}) => {
+    return request('GET', url, options).getBody('utf8')
+};
+
+/**
+ * [function] 同步执行request post
+ * @param {string} url
+ * @param {object} options
+ * @return {object} body
+ */
+const requestPost = (url, options) => {
+    return JSON.parse(request('POST', url, options).getBody('utf8'))
+};
+
+export {
+    deepCopy,
+    requestGet,
+    requestGetRaw,
+    requestPost
+}
