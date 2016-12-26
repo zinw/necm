a0b3da010cc3c418b7d2f6e1cb4542b632acdb39
Zinway Liu <zinway.liu@ucloud.cn>
2016-12-26 23:39:55 +0800

    use `mpg123 -R`

--- src/player.js
+++ src/player.js
@@ -3,10 +3,7 @@
  */
 
 import EventEmitter from 'events'
-import http from 'http'
-import lame from 'lame'
-import Speaker from 'audio-speaker/stream'
-import MemoryStream from 'memorystream'
+import MpgPlayer from 'mpg123'
 import api from './api'
 
 /**
@@ -16,27 +13,22 @@ import api from './api'
 class Player extends EventEmitter {
     constructor(song_id_list) {
         super();
+        this.player = new MpgPlayer();
         this._list = song_id_list instanceof Array ? song_id_list : [];
     }
 
     play(index = 0) {
-        if (index >= this._list.length) {
+        if (typeof index !== 'number' || index < 0) {
+            return this
+        } else if (index >= this._list.length) {
             this.emit('finish', 'Playlist is over.');
             return this
         }
-        let song_id = this._list[index];
-        let ms = new MemoryStream();
-        http.get(api.getMp3UrlById(song_id), res => {
-            res.pipe(ms)
-        });
-        ms.once('data', () => {
-            ms
-                .pipe(new lame.Decoder())
-                .pipe(new Speaker())
-        });
-        ms.once('end', () => {
-            console.log(`played ${index}`);
-            //this.play(index + 1)
+        this.emit('playing', index);
+        let url = api.getMp3UrlById(this._list[index]);
+        this.player.play(url);
+        this.player.once('end', () => {
+            this.play(index + 1)
         });
     }
 }
