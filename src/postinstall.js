77dbe75423dd8f7140fd9f6e3e28fb3808951a23
Zinway Liu <zinway.liu@ucloud.cn>
2016-12-29 17:47:12 +0800

    add tips

--- src/postinstall.js
+++ src/postinstall.js
@@ -1,5 +1,13 @@
 /**
  * Created by Zinway on 2016/12/26.
  */
+import colors from 'colors'
 
-console.log('Make sure you have installed mpg123 command line audio player.');
+console.log('Make sure you have installed mpg123 command line audio player.'.rainbow);
+console.log('\nOn Ubuntu/Debian'.yellow.italic);
+console.log('sudo apt-get install mpg123'.green.bold);
+console.log('\nOn Arch Linux'.yellow.italic);
+console.log('sudo pacman -Sy mpg123'.green.bold);
+console.log('\nOn OSX'.yellow.italic);
+console.log('brew install mpg123'.green.bold);
+console.log();
