1f0b1dd6f61c49fb931db8ac24345ce9213deb04
Zinway Liu <zinway.liu@ucloud.cn>
2016-12-29 16:07:50 +0800

    init bin/necm

---
+++ index.js
@@ -0,0 +1,1 @@
+dist/necm.js
--- src/necm.js
+++ src/necm.js
@@ -1,11 +1,65 @@
 /**
- * Created by Zinway on 2016/12/9.
+ * Created by Zinway on 2016/12/19.
  */
-import Menu from './menu'
+import Ui from './ui'
+import api from './api'
+import Player from './player'
 
-class NECM extends Menu {
+class NECM extends Ui {
     constructor() {
         super();
+        this._bind('initTopList', 'topListDo');
+        this.mainMenuList = [
+            ['排行榜', this.initTopList]
+        ];
+        this.initMainMenu()
+    }
+
+    getMainMenuListNames() {
+        return this.mainMenuList.map((l, i) => ` ${i}. ${l[0]}`)
+    };
+
+    mainMenuListDo(index = 0) {
+        this.mainMenuList[index][1]();
+    }
+
+    initMainMenu() {
+        this.list.setItems(this.getMainMenuListNames());
+        this.screen.render();
+        this.list.once('select', (item, index) => {
+            this.list.setLabel(` ${item.getText().split('.').slice(-1)[0].trim()} `);
+            this.mainMenuListDo(index);
+            this.screen.render();
+        });
+    }
+
+    topListDo(index = 0) {
+        this.playList = api.getTopSongList(index);
+        this.songNames = this.playList.map((s, i) => {
+            const {name, artists, album} = s;
+            let _artists = artists.map(a => a.name).join(' & '),
+                _album = album.name;
+            return ` ${i}. ${name} - ${_artists} <${_album}>`;
+        });
+        this.list.setItems(this.songNames);
+        this.screen.render();
+        this.songIdList = this.playList.map((s) => s.id);
+        let player = new Player(this.songIdList);
+        this.list.on('select', (item, index) => {
+            player.play(index)
+        });
+        player.on('playing', index => this.list.select(index));
+    }
+
+    initTopList() {
+        this.list.setItems(api.getTopListNames());
+        this.screen.render();
+        this.list.once('select', async(item, index) => {
+            let title = ` ${item.getText().split('.').slice(-1)[0].trim()} `;
+            await this.topListDo(index);
+            this.list.setLabel(title);
+            this.screen.render();
+        });
     }
 }
 
